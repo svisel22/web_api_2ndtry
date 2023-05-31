7e5d78958c737f1dd5bd96e57e1159516faf3d2d
svisel22 <s_visel22@stud.hwr-berlin.de>
2023-05-31 14:43:33 +0200

    localhost works

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import requests
-#import json
-
+import pandas as pd
 
 response = requests.get("http://api.open-notify.org/astros.json")
 
@@ -18,5 +17,26 @@ if response.status_code == 200:
     st.write("Names of people in space:")
     for person in people:
         st.write(person["name"])
+
+    # Fetch the current location of the ISS
+    location_response = requests.get("http://api.open-notify.org/iss-now.json")
+
+    # Check if the location request was successful
+    if location_response.status_code == 200:
+        location_data = location_response.json()
+        latitude = float(location_data["iss_position"]["latitude"])
+        longitude = float(location_data["iss_position"]["longitude"])
+
+        # Create a dataframe with the latitude and longitude values
+        df = pd.DataFrame({"LAT": [latitude], "LON": [longitude]})
+
+        # Display the map with the current ISS location
+        st.write("Map showing the current location of the ISS:")
+        st.map(df)
+        
+        # Add a short description for the map
+        st.write("The marker on the map represents the current location of the International Space Station.")
+    else:
+        st.write("Error: Unable to retrieve ISS location data from the API")
 else:
     st.write("Error: Unable to retrieve data from the API")

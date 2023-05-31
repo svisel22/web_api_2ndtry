361f0e0ebd16621f52ab62e8b8ce3a76887953fa
svisel22 <s_visel22@stud.hwr-berlin.de>
2023-05-31 11:38:50 +0200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+# Use an official Python runtime as a parent image
+FROM python:3.10-slim-buster
+
+# Set the working directory to /app
+WORKDIR /app
+
+# Copy the requirements.txt file to the Docker image
+COPY app/requirements.txt .
+
+# Install the Python dependencies
+RUN pip install -r requirements.txt
+
+# Add any necessary system dependencies (if required)
+
+# Copy the application code to the Docker image
+COPY app .
+
+# Make port 8502 available to the world outside this container
+EXPOSE 8502
+
+# Set the entry point command to run the application
+CMD ["streamlit", "run", "main.py", "--server.port=8502", "--server.address=0.0.0.0"]
---
+++ app/streamlit.py
@@ -0,0 +1,22 @@
+import app.streamlit as st
+import requests
+import json
+
+
+response = requests.get("http://api.open-notify.org/astros.json")
+
+# Check if the request was successful
+if response.status_code == 200:
+    data = response.json()
+    number_of_people = data["number"]
+    people = data["people"]
+
+    # Display the total number of people in space
+    st.write("Total number of people in space:", number_of_people)
+
+    # Display the names of the people in space
+    st.write("Names of people in space:")
+    for person in people:
+        st.write(person["name"])
+else:
+    st.write("Error: Unable to retrieve data from the API")
